03daf0537c77b15496242bf78bad18f6896873b5
fukawa <ta1fukawa@gmail.com>
2021-08-13 02:26:16 +0900

    Update code

--- python/models.py
+++ python/models.py
@@ -4,8 +4,8 @@ import torch
 class EmbedModel1d(torch.nn.Module):
     def __init__(self, sp_length):
         super(EmbedModel1d, self).__init__()
-        self.conv1a = torch.nn.Conv1d(sp_length, 512, kernel_size=5, stride=1, dilation=1, padding='same')
-        self.conv1b = torch.nn.Conv1d(512, 512, kernel_size=5, stride=1, dilation=1, padding='same')
+        self.conv1a = torch.nn.Conv1d(sp_length, 512, kernel_size=3, stride=1, dilation=1, padding='same')
+        self.conv1b = torch.nn.Conv1d(512, 512, kernel_size=3, stride=1, dilation=1, padding='same')
         self.drop1  = torch.nn.Dropout(p=0.2)
         self.conv2a = torch.nn.Conv1d(512, 512, kernel_size=3, stride=1, dilation=2, padding='same')
         self.conv2b = torch.nn.Conv1d(512, 512, kernel_size=3, stride=1, dilation=1, padding='same')
@@ -13,12 +13,12 @@ class EmbedModel1d(torch.nn.Module):
         self.conv3a = torch.nn.Conv1d(512, 512, kernel_size=3, stride=1, dilation=3, padding='same')
         self.conv3b = torch.nn.Conv1d(512, 512, kernel_size=3, stride=1, dilation=1, padding='same')
         self.drop3  = torch.nn.Dropout(p=0.2)
-        self.conv4a = torch.nn.Conv1d(512, 512, kernel_size=1, stride=1, dilation=1, padding='same')
-        self.conv4b = torch.nn.Conv1d(512, 512, kernel_size=1, stride=1, dilation=1, padding='same')
+        self.conv4a = torch.nn.Conv1d(512, 512, kernel_size=3, stride=1, dilation=1, padding='same')
+        self.conv4b = torch.nn.Conv1d(512, 512, kernel_size=3, stride=1, dilation=1, padding='same')
         self.drop4  = torch.nn.Dropout(p=0.2)
-        self.conv5  = torch.nn.Conv1d(512, 1500, kernel_size=1, stride=1, dilation=1, padding='same')
+        self.conv5  = torch.nn.Conv1d(512, 2048, kernel_size=3, stride=1, dilation=1, padding='same')
         self.drop5  = torch.nn.Dropout(p=0.2)
-        self.line1  = torch.nn.Linear(3000, 512)
+        self.line1  = torch.nn.Linear(4096, 512)
 
     def _stats_pooling(self, x):
         mean = torch.mean(x, dim=2)
