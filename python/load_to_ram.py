5f21f3597dffd0e10a3b0c311d2fa9c37d5797fa
fukawa <ta1fukawa@gmail.com>
2021-09-03 01:30:04 +0900

    Update

--- python/load_to_ram.py
+++ python/load_to_ram.py
@@ -10,34 +10,13 @@ person_unknown_size = 16
 voice_train_size = 64
 voice_check_size = 8
 
-person_no_list = np.array([8, 16, 17, 21, 23, 29, 35, 37, 42, 46, 47, 50, 54, 58, 59, 73, 88, 97])
-voice_no_list  = np.array([5, 18, 24, 40, 42, 44, 46, 55, 56, 59, 60, 61, 63, 65, 71, 73, 75, 81, 84, 85, 87, 93, 94, 98])
-
-def calc_file_idx(no_list, base_size):
-    last_append_size = 0
-    while True:
-        append_size = np.count_nonzero(no_list < base_size + last_append_size)
-        if append_size == last_append_size:
-            break
-        last_append_size = append_size
-    return base_size + last_append_size
-
-person_known_idx   = calc_file_idx(person_no_list, person_known_size)
-person_unknown_idx = calc_file_idx(person_no_list, person_known_size + person_unknown_size)
-voice_train_idx    = calc_file_idx(voice_no_list, voice_train_size)
-voice_check_idx    = calc_file_idx(voice_no_list, voice_train_size + voice_check_size)
-
-known_person_list   = list(filter(lambda x:x not in person_no_list, np.arange(person_known_idx)))
-# unknown_person_list = list(filter(lambda x:x not in person_no_list, np.arange(person_known_idx, person_unknown_idx)))
-voice_list          = list(filter(lambda x:x not in voice_no_list, np.arange(voice_check_idx)))
-
-for person in known_person_list:
+for person in range(person_known_size + person_unknown_size):
     
     print('[Processing] person:', person + 1)
     
     os.makedirs(os.path.split(dst)[0] % {'person': person + 1}, exist_ok=True)
 
-    for voice in voice_list:
+    for voice in range(voice_train_size + voice_check_size):
 
         specific = { 'person': person + 1, 'voice': voice + 1 }
 
